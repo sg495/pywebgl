0180e11f0fceaff605439ba821cc30297b5a9ebd
sg495 <sg495@users.noreply.github.com>
2019-08-25 18:24:32 +0100

    Enabled sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -53,3 +53,4 @@ html_static_path = ['_static']
 
 # https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
 master_doc = 'index'
+
